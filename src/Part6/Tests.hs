123b29e2c5d24fdcdac8becdea03320abe72264d
DarkHole1 <dark.hole1@yandex.ru>
2024-12-23 13:53:42 +0300

    Add matrix tests

--- src/Part6/Tests.hs
+++ src/Part6/Tests.hs
@@ -22,3 +22,26 @@ unit_zero = do
     zero 5 5 @?= SparseMatrix 5 5 (Data.Map.fromList ([]::[((Int, Int), Int)]))
     where zz :: Int; zz = 0
 
+unit_multiplyMatrix = do
+    multiplyMatrix a b @?= c
+    where
+        a :: [[Int]]
+        a = [[1, 0, 1], 
+            [2, 1, 1], 
+            [0, 1, 1], 
+            [1, 1, 2]]
+        b :: [[Int]]
+        b = [[1, 2, 1],
+            [2, 3, 1],
+            [4, 2, 2]]
+        c :: [[Int]]
+        c = [[5, 4, 3],
+            [8, 9, 5],
+            [6, 5, 3],
+            [11, 9, 6]]
+
+unit_determinant = do
+    determinant (10 :: Int) @?= (10 :: Int)
+    determinant ([[1, 2], [3, 4]] :: [[Int]]) @?= (-2 :: Int)
+    determinant ([[1, 2, 3], [4, 5, 6], [7, 8, 9]] :: [[Int]]) @?= (0 :: Int)
+    determinant (eye 10 :: [[Int]]) @?= 1
